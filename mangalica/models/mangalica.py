b0a029ba8567b46dd8eca1dbd00c70173f666237
Kevin Ye <yekevin12@gmail.com>
2024-06-24 13:05:08 +0200

    prepare for coca trainer

--- mangalica/models/mangalica.py
+++ mangalica/models/mangalica.py
@@ -8,18 +8,24 @@ class MangaliCaConfig():
              image_dim: int = 768,
              num_img_query: int = 256,
              caption_loss_weight: int = 1.0,
-             contrastive_loss_weight: int = 1.0):
-                 
+             contrastive_loss_weight: int = 1.0,
+             temperature: float = 0.07
+             ):
+
         self.text_dim = text_dim
-        self.img_dim = img_dim
+        self.img_dim = image_dim
         self.num_img_query = num_img_query
+        self.caption_loss_weight = caption_loss_weight
+        self.contrastive_loss_weight = contrastive_loss_weight
+        self.temperature = temperature
 
 class MangaliCa(nn.Module):
     def __init__(self,
+                 config: MangaliCaConfig,
                  img_encoder = None,
                  unimodal_decoder = None,
                  multimodal_decoder = None,
-                 config: MangaliCaConfig):
+                 ):
         self.model_config = config
 
         self.img_encoder = img_encoder
--- mangalica/models/utils.py
+++ mangalica/models/utils.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from einops import rearrange, einsum
 
 from typing import Optional
 
@@ -78,13 +79,74 @@ class PrepareLlamaForTrainer(nn.Module):
                 input_ids: torch.Tensor,
                 labels: Optional[torch.Tensor] = None,
                 attention_mask: Optional[torch.Tensor] = None):
-
         logits = self.model(input_ids)
         return {
                 'logits': logits,
                 'loss': self.compute_loss(logits.float(), input_ids)
             }
 
+
+class PrepareForCoCaTrainer(nn.Module):
+    """
+    To make the model compatible with HuggingFace's Trainer the model should
+    produce a dictionary containing the 'logits' and 'loss'.
+
+    This class implements a custom loss computation and forward pass.
+    """
+
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+        self.ce_loss_fn = F.cross_entropy
+
+    def compute_ca_loss(self, logits, labels):
+        shift_logits = logits[..., :-1, :].contiguous()
+        shift_labels = labels[..., 1:].contiguous()
+        shift_logits = shift_logits.view(-1, self.model.n_tokens)
+        shift_labels = shift_labels.view(-1).to(torch.long)
+        #logits = rearrange(logits, 'b n c -> b c n')
+        ce = self.ce_loss_fn(shift_logits, shift_labels)
+        ca_weight =  self.model.model_config.caption_loss_weight
+        ca = ce * ca_weight
+        return ca
+
+    def compute_co_loss(self, text_latents, image_latents):
+        # calculate contrastive loss
+        sim = einsum('i d, j d -> i j', text_latents, image_latents)
+        sim = sim * self.model.model_config.temperature.exp()
+        contrastive_labels = torch.arange(text_latents.shape[0], device=text_latents.device)
+
+        contrastive_loss = (self.ce_loss_fn(sim, contrastive_labels) + self.ce_loss_fn(sim.t(), contrastive_labels)) * 0.5
+        contrastive_loss = contrastive_loss * self.model.model_config.contrastive_loss_weight
+        return contrastive_loss
+
+    def compute_loss(self, logits, labels, text_latents, image_latents):
+        captioning_loss = self.compute_ca_loss(logits, labels)
+        contrastive_loss = self.compute_co_loss(text_latents, image_latents)
+
+        return captioning_loss+contrastive_loss
+
+    def print_trainable_parameters(self):
+        trainable_params, all_params = 0, 0
+        trainable_params = sum(param.numel() for param in self.image_model.parameters() if param.requires_grad) + \
+                sum(param.numel() for param in self.language_model.parameters() if param.requires_grad)
+
+        all_params = sum(param.numel() for param in self.image_model.parameters()) + \
+                sum(param.numel() for param in self.language_model.parameters())
+        print(f'trainable params: {trainable_params:,} | all params: {all_params:,} | trainable: {100 * trainable_params / all_params}%')
+
+    def forward(self,
+            logits: torch.Tensor,
+            labels: torch.Tensor,
+            ):
+
+            llm_logits = self.language_model(input_ids)
+            img_logits = self.image_model(pixel_values)
+            return {
+                    'llm_logits': llm_logits,
+                    'logits': logits,
+                    'loss': 
+                }
 
 '''
 Apply Embedding to Latents before passing them into the Multimodal Network
